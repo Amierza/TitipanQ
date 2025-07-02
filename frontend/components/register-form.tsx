95af8ebb3e393e4e4cee2194aee2ff8d6106c010
Reyysusanto <rheinaldysusanto@gmail.com>
2025-07-02 23:48:00 +0700

    integrate auth with backend

--- frontend/components/register-form.tsx
+++ frontend/components/register-form.tsx
@@ -1,62 +1,178 @@
-"use client"
+"use client";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import Image from "next/image"
-import Link from "next/link"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { RegisterSchema } from "@/validation/auth.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useMutation } from "@tanstack/react-query";
+import { registerUserService } from "@/services/auth/registerUserService";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export function RegisterForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const [show, setShow] = useState(false);
+  const router = useRouter();
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      user_name: "",
+      user_email: "",
+      user_phone_number: "",
+      user_password: "",
+    },
+  });
+
+  const { mutate: registerUser, isPending } = useMutation({
+    mutationFn: registerUserService,
+    onSuccess: (result) => {
+      if (result.status) {
+        toast.success(result.message);
+        router.push("/login");
+      } else {
+        toast.error(result.message);
+      }
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
+    console.log("Submitted values:", values);
+    registerUser(values);
+  };
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          {/* Form Section */}
-          <form className="p-6 md:p-8 space-y-6">
-            <div className="text-center">
-              <h1 className="text-2xl font-bold">Create an account</h1>
-              <p className="text-muted-foreground text-sm">
-                Register your account to use TitipanQ
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="p-6 md:p-8 space-y-6"
+            >
+              <div className="text-center">
+                <h1 className="text-2xl font-bold">Create an account</h1>
+                <p className="text-muted-foreground text-sm">
+                  Register your account to use TitipanQ
+                </p>
+              </div>
+
+              <FormField
+                control={form.control}
+                name="user_name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="m@example.com"
+                        type="text"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="user_email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="m@example.com"
+                        type="email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="user_phone_number"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Phone Number</FormLabel>
+                    <FormControl>
+                      <Input placeholder="08xxxxxxxxxx" type="tel" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="user_password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Input
+                          {...field}
+                          id="password"
+                          placeholder="******"
+                          type={show ? "text" : "password"}
+                          required
+                        />
+                        <Button
+                          type="button"
+                          variant="ghost"
+                          onClick={() => setShow((prev) => !prev)}
+                          className="absolute right-2 top-0 p-0 m-0 bg-transparent border-none shadow-none ring-0 focus:ring-0 focus-visible:ring-0 hover:bg-transparent"
+                          tabIndex={-1}
+                        >
+                          {show ? (
+                            <Eye size={12} className="text-black" />
+                          ) : (
+                            <EyeOff size={12} className="text-black" />
+                          )}
+                        </Button>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" disabled={isPending} className="w-full">
+                {isPending ? "Loading..." : "Register"}
+              </Button>
+              <p className="text-center text-sm text-muted-foreground">
+                Sudah punya akun?{" "}
+                <Link href="/login" className="underline underline-offset-4">
+                  Login
+                </Link>
               </p>
-            </div>
-
-            <div className="space-y-4">
-              <div className="grid gap-2">
-                <Label htmlFor="name">Nama</Label>
-                <Input id="name" type="text" placeholder="Nama lengkap" required />
-              </div>
-
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" placeholder="m@example.com" required />
-              </div>
-
-              <div className="grid gap-2">
-                <Label htmlFor="no_telp">No. Telepon</Label>
-                <Input id="no_telp" type="tel" placeholder="08xxxxxxxxxx" required />
-              </div>
-
-              <div className="grid gap-2">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" required />
-              </div>
-            </div>
-
-            <Button type="submit" className="w-full">
-              Register
-            </Button>
-            <p className="text-center text-sm text-muted-foreground">
-              Sudah punya akun?{" "}
-              <Link href="/login" className="underline underline-offset-4">
-                Login
-              </Link>
-            </p>
-          </form>
+            </form>
+          </Form>
 
           {/* Image Section */}
           <div className="relative hidden md:block bg-muted">
@@ -70,5 +186,5 @@ export function RegisterForm({
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
