4ce51d95faed38f13f291e2d0b45ba21c5eb8485
mferdian <eikhapoetra@gmail.com>
2025-07-08 10:59:21 +0700

    fetch user data

--- frontend/types/package.ts
+++ frontend/types/package.ts
@@ -1,14 +1,18 @@
 export type PackageType = "document" | "package" | "other";
 
 export interface PackageItem {
-  id: string;
-  userId: string;
-  userName: string;
-  type: PackageType;
-  description: string;
-  photoUrl?: string;
+  package_id: string;
+  package_description: string;
+  package_image: string;
+  package_type: string;
+  package_status: "received" | "delivered" | "completed" | "expired";
+  package_delivered_at: string;
+  package_expired_at: string;
+  user_id: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string | null;
 }
-
 
 export interface PackageFormData {
   userId: string;
